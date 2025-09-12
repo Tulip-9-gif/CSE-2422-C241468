a2dca1f8b8932a836064cc84bbac1c1f3e6fa4cd
Ahana Das ID-C241468 <ahanadas.bd.81@gmail.com>
2025-09-13 00:22:48 +0600

    Add files via upload

---
+++ Bubble sort.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+void bubblesort(int arr[],int n)
+{
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                int temp;
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+}
+void AssendingPrint(int arr[],int n)
+{
+    cout<<"Assending order: ";
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+int main(){
+    cout<<"enter number of elements: ";
+    int n;
+    cin>>n;
+    int arr[n];
+    cout<<"enter elements: ";
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    bubblesort(arr,n);
+    AssendingPrint(arr,n);
+    return 0;
+}
