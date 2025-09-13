3a580cefaa84c14d143ac34524f2eb2a74c200b0
Ahana Das ID-C241468 <ahanadas.bd.81@gmail.com>
2025-09-13 19:22:31 +0600

    Bubble sort using flag.cpp

--- Bubble sort.cpp
+++ Bubble sort.cpp
@@ -3,6 +3,7 @@ using namespace std;
 void bubblesort(int arr[],int n)
 {
     for(int i=0;i<n-1;i++){
+            int flag=0;
         for(int j=0;j<n-i-1;j++)
         {
             if(arr[j]>arr[j+1])
@@ -11,8 +12,10 @@ void bubblesort(int arr[],int n)
                 temp=arr[j];
                 arr[j]=arr[j+1];
                 arr[j+1]=temp;
+          flag = 1;
             }
         }
+       if(flag==0){ break; }
     }
 }
 void AssendingPrint(int arr[],int n)
